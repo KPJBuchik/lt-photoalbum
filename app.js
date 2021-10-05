a539b22bea846ed7c2690810f0293a04e8c0df7f
Kevin Buchik <kbuchik@gmail.com>
2021-10-04 17:06:49 -0700

    add graceful exit

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
 const inquirer = require("inquirer");
-
 const axios = require('axios');
 
-const start = async (userInput) => {
+start();
+
+function start(userInput) {
 
     inquirer.prompt([
 
         {
             name: "userInput",
             type: "input",
-            message: "select the album id by number 1-100",
+            message: "Select the album id by number 1-100. q to exit",
             validate: function (id) {
                 userInput = id
                 if (!isNaN(id) && id != 0 && id < 101) {
                     return true;
                 }
+                if (id === "q"){
+                    process.exit(0)
+                }
+
                 return false;
             }
         }
 
     ]).then(function () {
-
+        //retrive json data 
         axios.get(`https://jsonplaceholder.typicode.com/photos?albumId=${userInput}`).then(
             function (response) {
+                //loop through specified album id 
                 for (let i = 0; i < response.data.length; i++) {
-    
+
                     console.log("[" + response.data[i].id + "]" + " " + response.data[i].title);
                     console.log(" ");
                 }
-
+                //catch error if connection cannot be made to api    
             }).catch(function (error) {
                 console.log(error);
             })
@@ -40,18 +46,15 @@ const start = async (userInput) => {
 
     })
 }
-start();
 
 
 
+class Album {
+    static async all() {
 
-
-
-// class Album {
-//     static async all() {
-//         let res = await axios.get('https://jsonplaceholder.typicode.com/photos?albumId=3');
-//         return res.data
-//     }
-// }
-// module.exports = Album;
+        let res = await axios.get('https://jsonplaceholder.typicode.com/photos?albumId=3');
+        return res.data
+    }
+}
+module.exports = Album;
 module.exports = start;
--- app.test.js
+++ app.test.js
@@ -1,81 +1,6 @@
-// const start = require('./app');
-// const axios = require('axios');
-
-// jest.mock('axios');
-
-// it('returns id and title of image', async () => {
-//   axios.get.mockResolvedValue({
-//     data: [
-//       {
-//         albumId: 3,
-//         id: 1,
-//         title: 'accusamus beatae ad facilis cum similique qui sunt',
-//         url: "https://via.placeholder.com/600/e743b",
-//         thumbnailUrl: "https://via.placeholder.com/150/e743b"
-//       },
-//       {
-//         albumId: 3,
-//         id: 2,
-//         title: 'reprehenderit est deserunt velit ipsam',
-//         url: "https://via.placeholder.com/600/e743b",
-//         thumbnailUrl: "https://via.placeholder.com/150/e743b"
-
-//       }
-//     ]
-//   });
-
-//   const idTitle =  start(3);
-//   expect(idTitle).toEqual('[1] accusamus beatae ad facilis cum similique qui sunt');
-
-// });
-
-// const axios = require('axios');
-
-// const start = require('./app');
-
-// jest.mock("axios");
-
-// describe("start", () => {
-//   describe("when API call is successful", () => {
-//     it("should return photo id and titles", async () => {
-//       // given
-//       const data = [
-//         {
-//                   albumId: 3,
-//                   id: 1,
-//                   title: 'accusamus beatae ad facilis cum similique qui sunt',
-//                   url: "https://via.placeholder.com/600/e743b",
-//                   thumbnailUrl: "https://via.placeholder.com/150/e743b"
-//                 },
-//                 {
-//                   albumId: 3,
-//                   id: 2,
-//                   title: 'reprehenderit est deserunt velit ipsam',
-//                   url: "https://via.placeholder.com/600/e743b",
-//                   thumbnailUrl: "https://via.placeholder.com/150/e743b"
-
-//                 }
-//       ];
-//       axios.get.mockResolvedValueOnce(data);
-
-//       // when
-//       const result = await start(3);
-
-//       // then
-//       // expect(result).toEqual(data);
-//         expect(result).toEqual('[1] accusamus beatae ad facilis cum similique qui sunt');
-
-//     });
-//   });
-
-// });
-
-
-
 const axios = require('axios');
-
 jest.mock('axios');
-
+//tests json structure of API
 test('should return id and photo title', () => {
 
   const album = [{
@@ -85,6 +10,7 @@ test('should return id and photo title', () => {
     url: "https://via.placeholder.com/600/e743b",
     thumbnailUrl: "https://via.placeholder.com/150/e743b"
   },
+
   {
     albumId: 3,
     id: 2,
@@ -98,11 +24,36 @@ test('should return id and photo title', () => {
   const response = { data: album };
 
   axios.get.mockResolvedValue(response);
-  axios.get.mockImplementation(() => Promise.resolve(response));
+
   for (let i = 0; i < response.data.length; i++) {
 
-    console.log("[" + response.data[i].id + "]" + " " + response.data[i].title)
+     console.log("[" + response.data[i].id + "]" + " " + response.data[i].title)
 
   }
 
 });
+
+test('should return undefined ', () => {
+
+  const album = [{
+    url: "https://via.placeholder.com/600/e743b",
+    thumbnailUrl: "https://via.placeholder.com/150/e743b"
+  },
+  {
+    url: "https://via.placeholder.com/600/e743b",
+    thumbnailUrl: "https://via.placeholder.com/150/e743b"
+
+  }
+  ];
+
+  const response = { data: album };
+
+  axios.get.mockResolvedValue(response);
+  for (let i = 0; i < response.data.length; i++) {
+
+     console.log("[" + response.data[i].id + "]" + " " + response.data[i].title)
+
+  }
+
+});
+
