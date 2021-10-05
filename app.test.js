d0c2120c6b5afb22625b64981fe52003ab7ba888
Kevin Buchik <kbuchik@gmail.com>
2021-10-04 20:32:06 -0700

    jest test

--- app.test.js
+++ app.test.js
@@ -1,44 +1,33 @@
+jest.mock('axios');
+
 const axios = require('axios');
+const  run  = require('./console');
 
-const BASE_URL = "https://jsonplaceholder.typicode.com/photos?albumId=";
+it(' return mock album data from axios get request', async () => {
+  const id = 3;
+  const album = [
+                  {
+              albumId: 3,
+              id: 1,
+              title: 'accusamus beatae ad facilis cum similique qui sunt',
+              url: "https://via.placeholder.com/600/e743b",
+              thumbnailUrl: "https://via.placeholder.com/150/e743b"
+            },{
+              albumId: 3,
+              id: 1,
+              title: 'accusamus beatae ad facilis cum similique qui sunt',
+              url: "https://via.placeholder.com/600/e743b",
+              thumbnailUrl: "https://via.placeholder.com/150/e743b"
+            }
+    
+            
+    
+          ]
 
-const run = require('./console');
+  axios.get.mockResolvedValue(album);
 
-jest.mock("axios");
-describe("run", () => {
-  describe("when get request is successful", () => {
-    it("should return endpoint", async () => {
-      // dummy data
-      const album = [
-              {
-          albumId: 3,
-          id: 1,
-          title: 'accusamus beatae ad facilis cum similique qui sunt',
-          url: "https://via.placeholder.com/600/e743b",
-          thumbnailUrl: "https://via.placeholder.com/150/e743b"
-        }
+  const response = run(3);
 
-      ];
-      axios.get.mockResolvedValueOnce(`${BASE_URL}`);
-
-      // when
-      const result =  run(3);
-
-      // then
-      expect(axios.get).toHaveBeenCalledWith(`${BASE_URL}3`);
-      expect(result).toEqual(album);
-    });
-  });
-
-  describe("when get request call fails", () => {
-    it("should return empty array", async () => {
-      const message = "Network Error";
-      axios.get.mockRejectedValueOnce(new Error(message));
-
-      const result = await run();
-
-      expect(axios.get).toHaveBeenCalledWith(`${BASE_URL}3`);
-      expect(result).toEqual([]);
-    });
-  });
-});
+  expect(axios.get).toHaveBeenCalledTimes(1);
+  expect(axios.get).toHaveBeenCalledWith(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`);
+})
